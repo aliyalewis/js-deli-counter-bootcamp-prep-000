48e69d33404aa95ca2a3b6f34855565440e9c674
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-09 18:31:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine >0) {
+  if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine.shift()}.`
   } else {
     return "There is nobody waiting to be served!"
